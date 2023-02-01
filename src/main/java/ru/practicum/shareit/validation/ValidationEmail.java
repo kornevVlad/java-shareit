565f7c208c36ca2463c90ec4a8d182885af178ee
vladKornev <vlad1190@yandex.ru>
2023-02-01 17:32:04 +0300

    Локальное прохождение тестов Postman 1.7(Удален validEmail)

--- src/main/java/ru/practicum/shareit/validation/ValidationEmail.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.validation;
-
-public class ValidationEmail extends RuntimeException {
-
-    public ValidationEmail(String message) {
-        super(message);
-    }
-}
